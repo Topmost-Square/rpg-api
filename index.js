aae087f4a44e94e1a495570d369ddef512696862
keithchasen <keithchasen89@gmail.com>
2023-05-26 19:54:38 +0200

    added dotenv

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-const port = 7777;
+const port = process.env.PORT || 1111;
 app.listen(port, () => {
-    console.log(`API server is running`);
+    console.log(`API server is running on port ${port}`);
 });
 
